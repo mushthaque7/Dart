9438680df7c4ec0bf4346d41215e8e3e8c243c86
mushthaque7 <107250885+mushthaque7@users.noreply.github.com>
2024-07-06 16:04:54 +0530

    navigation

--- main.dart
+++ main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-main() {
+void main() {
   runApp(const MyApp());
 }
 
@@ -10,81 +10,73 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Login screen"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Login',
-              style: TextStyle(
-                  fontSize: 35,
-                  color: Colors.teal,
-                  fontWeight: FontWeight.bold),
+      theme: ThemeData(primaryColor: Colors.purple),
+      home: const IntroPage(),
+      routes: {
+        '/homepage': (context) => HomePage(),
+        '/intropage': (context) => IntroPage(),
+      },
+    );
+  }
+}
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('I N T R O P A G E'),
+        backgroundColor: Colors.purple,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.purple,
+            textStyle: TextStyle(
+              color: Colors.white,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30),
-              child: Form(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      child: TextFormField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          hintText: 'Enter Email',
-                          prefixIcon: Icon(Icons.email),
-                          border: OutlineInputBorder(),
-                        ),
-                        onChanged: (String value) {},
-                        validator: (value) {
-                          return value!.isEmpty ? "Please enter email" : null;
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      child: TextFormField(
-                        keyboardType: TextInputType.visiblePassword,
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          hintText: 'Enter Password',
-                          prefixIcon: Icon(Icons.password),
-                          border: OutlineInputBorder(),
-                        ),
-                        onChanged: (String value) {},
-                        validator: (value) {
-                          return value!.isEmpty
-                              ? "Please enter password"
-                              : null;
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 35),
-                      child: MaterialButton(
-                        onPressed: () {},
-                        minWidth: double.infinity,
-                        child: Text('Login'),
-                        color: Colors.teal,
-                        textColor: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
+          ),
+          child: const Text(
+            "Go to Home page",
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, '/homepage');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('H O M E P A G E'),
+          backgroundColor: Colors.purple,
+        ),
+        body: Center(
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.purple,
+            ),
+            child: const Text(
+              "Go to intro",
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, '/intropage');
+            },
+          ),
         ),
       ),
     );
