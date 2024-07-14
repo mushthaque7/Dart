67fd0e4a1c0b51613d1fa31015a534fbf1c6aec4
mushthaque7 <107250885+mushthaque7@users.noreply.github.com>
2024-07-14 15:44:14 +0530

    Add files via upload

---
+++ web.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Website',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  void _launchURL() async {
+    final Uri url = Uri.parse('https://flutter.dev/');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Website'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _launchURL,
+          child: Text('Website'),
+        ),
+      ),
+    );
+  }
+}
